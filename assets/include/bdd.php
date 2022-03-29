10d7c5f3f466b506fa7f433ccf42e51c396e1e06
epyion <christopher.sauvat@gmail.com>
2022-03-29 12:52:52 +0200

    pull

---
+++ assets/include/bdd.php
@@ -0,0 +1,10 @@
+<?php
+    $user = "root";
+    $pass = "";
+
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=tips;charset=utf8', $user, $pass);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
+    }
