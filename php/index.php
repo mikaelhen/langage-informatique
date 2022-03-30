9608e3ec0658884df5c7f46528936ed427f3d162
mikaelhen <mikaelhen28@gmail.com>
2022-03-30 11:27:56 +0200

    maj

--- php/index.php
+++ php/index.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../assets/css/style.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200&display=swap');
+  </style>
   <title>tips</title>
 </head>
 
@@ -14,7 +17,10 @@
     <?php include "../assets/include/navbar.php" ?>
   </header>
 
-  <div class="nav box"></div>
+  <div class="nav box">
+    <h1>TIPS Langage Informatique</h1>
+    <p>Bienvenu sur le site d'echange et de partage sur le code informatique.<br><br> Ce site est fait dans le but d'apporter ses idées, ses expériences, d'enrichir ses connaissances. </p>
+  </div>
 
   <div class="container">
     <article class="box1"><img src="../assets/img/html.jpg">
