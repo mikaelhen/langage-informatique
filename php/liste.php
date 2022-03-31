d68bf804629695ddc37f2061288f817a1cd22140
epyion <christopher.sauvat@gmail.com>
2022-03-31 15:06:50 +0200

    p

--- php/liste.php
+++ php/liste.php
@@ -16,7 +16,9 @@ require_once '../assets/include/bdd.php';
     <body>
         <?php include "../assets/include/navbar.php" ?>
         <?php
-        $sql = "SELECT * FROM tips WHERE id_tips";
+
+        $sql = "SELECT * FROM avoir a,categorie c,tips t WHERE a.id_tips = t.id_tips and c.id_categorie=a.id_categorie and c.id_categorie=".$_GET['id_tips']."";
+ 
         $requete = $bdd->prepare($sql);
         $requete->execute();
         $row = $requete->fetchAll();
@@ -28,7 +30,11 @@ require_once '../assets/include/bdd.php';
 
                 foreach ($row as &$tip) {
                 ?>
-                    <li><a href=""><?php echo $tip['titre_tips'] ?></a></li>
+                    <li>
+                        <a href="tips.php?id_tips=<?php echo $tip['titre_tips'] ?>">
+                        <?php echo $tip['titre_tips'] ?>
+                        </a>
+                    </li>
                 <?php
                 }
 
@@ -38,7 +44,8 @@ require_once '../assets/include/bdd.php';
         </div>
         <div class="container2">
             <button class="btn">
-                <b href="add_tips.php">Ajouter un tips</b>
+                <b href="add_tips.php">
+                    ajouter un tips</b>
             </button>
             <a class="btn" href="liste.php">Retour</a>
         </div>
