9eb7363f7e0d5665d7968bee2c41c41d8cc86a1f
baxter08 <baxter08@hotmail.fr>
2022-03-31 14:51:31 +0200

    maj

--- php/liste.php
+++ php/liste.php
@@ -38,7 +38,8 @@ require_once '../assets/include/bdd.php';
         </div>
         <div class="container2">
             <button class="btn">
-                <b href="add_tips.php">ajouter un tips</b>
+                <b href="add_tips.php">
+                    ajouter un tips</b>
             </button>
             <a class="btn" href="liste.php">Retour</a>
         </div>
