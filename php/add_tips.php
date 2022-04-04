df2ffd4b2230a2ffe76116a025339433bc227b2b
epyion <christopher.sauvat@gmail.com>
2022-04-04 11:41:18 +0200

    aie aie aie

--- php/add_tips.php
+++ php/add_tips.php
@@ -3,6 +3,7 @@
 // die(var_dump($_POST));
 session_start();
 require_once '../assets/include/bdd.php';;
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -115,9 +116,10 @@ if (empty($_GET['action']))
                             if ($nbcat >= '1') {
 
                                 ?>
-
+ <form action="add_tips.php?action" method="post">
                                 <div class="categorie">
                          <select name="id_categorie">
+                         <option value=""></option>
                         <?php
 
                                 while($l = $requeteCategorie->fetch())
@@ -134,52 +136,102 @@ if (empty($_GET['action']))
                         <?php 
                              } // fin de mon else if
 
-                             else if ($nbcat == '0') {}
-
+                             else if ($nbcat == '0') {
+                                 
+                             }
+                            }
+                            
 // ici creation de cat*
                              
 
 
                         ?>
-                        <form action="add_tips.php?action" method="post">
+                       
                         <div class="categorie">
-                        <input type="name" name="nom_categorie" placeholder="crée_categorie" value="<?php echo !empty($categorie) ? $categorie : ''; ?>">
+                        <input type="name" name="nom_categorie" placeholder="crée_categorie" value="">
                         </div>
+                        
                         <div class="titre">
-                        <input type="name" name="titre_tips" placeholder="titre" value="<?php echo !empty($titre) ? $titre : ''; ?>">
+                        <input type="name" name="titre_tips" placeholder="titre" value="">
                         </div>
                         <div class="detaille">
-                        <input type="name" name="detail_tips" placeholder="detail" value="<?php echo !empty($detail) ? $detail : ''; ?>">
+                        <input type="name" name="detail_tips" placeholder="detail" value="">
                         </div>
-                        <input type="submit" class="btn" name="submit" value="submit">
+                        <input type="submit" class="btn"  value="submit">
+                        <?php
+                        if (isset($_POST['id_langage'])) {
+                            ?>
+                        <input type="hidden" name="language" value="<?= $_POST['id_langage']  ?>">
+                        <?php
+                        }
+                        ?>
                         </form>
 
                         <?php
-                            //                     if (isset($_POST['submit'])) {
-                            //                         $sql = "INSERT INTO tips (titre_tips,detail_tips) values( :titre_tips, :detail_tips)";
-                            //                             $add = $bdd->prepare($sql);
-                            //                             $add->execute(array(
-                            //                                 ':titre_tips' => $titre,
-                            //                                 ':detail_tips' => $detail
+     
+                            if (isset($_POST['titre_tips'])) {
+                               
+
+
+
+                                   
+
+                                    $reqone = $bdd->prepare("INSERT INTO tips (titre_tips, detail_tips) VALUES (?,?)");
+                                    $reqone->execute(array($_POST['titre_tips'], $_POST['detail_tips']));
+                                    $tips = $bdd->lastInsertId();
+
+                                    var_dump($_POST['id_categorie']);
+                                    var_dump($_POST['nom_categorie']);
+                                    if ($_POST['id_categorie'] == ''){
+                                        echo 'ok';
+                                        if ($_POST['nom_categorie'] != ''){
+                                            echo 'encore plus ok';
+                                            var_dump($_POST['nom_categorie']);
+                                            $requete = $bdd->prepare("INSERT INTO categorie (nom_categorie, id_image) VALUES(?,?)");
+                                            $requete->execute(array($_POST['nom_categorie'], 'imagefictif'));
+
+                                            $categorie = $bdd->lastInsertId();
+                                            var_dump($_POST['language']);
+                                            var_dump($categorie);
+                                            $req = $bdd->prepare("INSERT INTO possede (id_categorie, id_langage) VALUES (?,?)");
+                                            $req->execute(array($categorie, $_POST['language']));
+                                        }else{
+                                            $categorie = $_POST['id_categorie'];
+                                        }
+                                    }else{
+                                        $categorie = $_POST['id_categorie'];
+                                    }
+                                    echo $categorie;
+                                    echo '</br>';
+                                    echo $tips;
+                                    $Req2 = $bdd->prepare("INSERT INTO avoir (id_tips, id_categorie) VALUES (?, ?)");
+                                    $Req2->execute(array($tips, $categorie));
+
+                                    // $sql = "INSERT INTO langage (nom_langage) values( :nom_langage)";
+                                    // $add = $bdd->prepare($sql);
+                                    // $add->execute(array(
+                                    //     ':nom_langage' => $l,
+                            
+                                    // ));
+                                    // $sql = "INSERT INTO categorie (nom_categorie) values( :nom_categorie)";
+                                    // $add = $bdd->prepare($sql);
+                                    // $add->execute(array(
+                                    //     ':nom_categorie' => $categorie,
+                                    // ));
+                            
+                                    // echo "votre tips a bien été ajouter";
+                                    // header("Location: liste.php");
                                                  
-                            // } // fin de mon action choix
+                            } // fin de mon action choix
 
 
-                        } // fin de isset get action
+                         // fin de isset get action
 
 
 
 
-    //     $sql = "INSERT INTO tips (titre_tips,detail_tips) values( :titre_tips, :detail_tips)";
-    //     $add = $bdd->prepare($sql);
-    //     $add->execute(array(
-    //         ':titre_tips' => $titre,
-    //         ':detail_tips' => $detail
-    //     ));
 
-    //     echo "votre tips a bien été ajouter";
-    //     // Database::disconnect();
-    //     header("Location: liste.php");
+      
     }
     ?>
 
