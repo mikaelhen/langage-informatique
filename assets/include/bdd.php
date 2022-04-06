2104edc1aee7bd3ffde63c64df193a3f96a56742
baxter08 <baxter08@hotmail.fr>
2022-04-06 10:30:30 +0200

    maj

--- assets/include/bdd.php
+++ assets/include/bdd.php
@@ -17,4 +17,20 @@ try {
         print "Erreur !: " . $e->getMessage() . "<br/>";
         die();
     }
-
+    ?>
+    
+    <?php
+    // Database configuration
+    $dbHost     = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "root";
+    $dbName     = "codexworld";
+    
+    // Create database connection
+    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+    ?>
