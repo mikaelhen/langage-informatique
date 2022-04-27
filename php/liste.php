772c81969c5b6f77bb7a181a7088e9a2b8ea2c5a
mikaelhen <mikaelhen28@gmail.com>
2022-04-27 11:45:55 +0200

    maj

--- php/liste.php
+++ php/liste.php
@@ -4,24 +4,26 @@ if (!isset($_SESSION['user'])) {
     header('location:index.php?login_err=pas_de_compte');
 } else {
 ?>
-    <!DOC TYPE html>
-        <html lang="fr">
 
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="../assets/css/style.css">
-            <link rel="stylesheet" href="../assets/css/navbar.css">
-            <link rel="stylesheet" href="../assets/css/footer.css">
-            <title>liste</title>
-        </head>
+    <!doctype html>
+    <html lang="fr">
 
-        <body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/navbar.css">
+        <link rel="stylesheet" href="../assets/css/footer.css">
+        <title>liste</title>
+    </head>
+
+    <body>
+        <header>
             <?php include "../assets/include/navbar.php" ?>
-
+        </header>
+        <div class="code">
             <?php
-
 
             $sql = "SELECT * FROM avoir a, categorie c, t_tips t WHERE a.id_tips = t.id_tips and c.id_categorie= a.id_categorie and c.id_categorie= " . $_GET['id_tips'] . "";
 
@@ -32,38 +34,40 @@ if (!isset($_SESSION['user'])) {
             ?>
 
             <h2>LES TIPS</h2>
-            <div class="container4">
-                <ul>
+        </div>
+        <div class="container4">
+            <ul>
 
-                    <?php
+                <?php
 
-                    foreach ($row as &$tip) {
-                    ?>
-                        <li>
-                            <a href="tips.php?id_tips=<?php echo $tip['id_tips'] ?>">
-                                <?php echo $tip['titre_tips'] ?>
-                            </a>
-                        </li>
-                    <?php
-                    }
+                foreach ($row as &$tip) {
+                ?>
+                    <li>
+                        <a href="tips.php?id_tips=<?php echo $tip['id_tips'] ?>">
+                            <?php echo $tip['titre_tips'] ?>
+                        </a>
+                    </li>
+                <?php
+                }
 
-                    ?>
+                ?>
 
-                </ul>
-            <?php } ?>
-            </div>
-            <div class="container2">
-                <button class="btn">
-                    <a href="add_tips.php">
-                        Ajouter un tips</a>
-                </button>
-                <button class="btn">
-                    <a href="index.php">Retour</a>
-                </button>
-            </div>
-            </div>
+            </ul>
+        <?php } ?>
+        </div>
+
+        <div class="container2">
+            <button class="btn">
+                <a href="add_tips.php">
+                    Ajouter un tips</a>
+            </button>
+            <button class="btn">
+                <a href="index.php">Retour</a>
+            </button>
+        </div>
+        <footer>
             <?php include "../assets/include/footer.php"; ?>
-            </div>
-        </body>
+        </footer>
+    </body>
 
-        </html>
+    </html>
