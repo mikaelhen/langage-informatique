760f883e09ab863b96d9ed4aca28e1edcdbdca3c
mikaelhen <mikaelhen28@gmail.com>
2022-04-29 14:43:23 +0200

    maj

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -1,4 +1,24 @@
-<footer class="footer box">
-    ©Copyright site réalisé par JP, Hassan, Chris, Mikaël
+<!DOCTYPE html>
+<html lang="en">
 
-</footer>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/f0fc4e252c.js" crossorigin="anonymous"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;1,200&display=swap');
+    </style>
+    <title>Document</title>
+</head>
+
+<body>
+    <footer class="footer box">
+        <div class="copy">
+            <p> ©Copyright site réalisé par JP, Hassan, Chris, Mikaël</p>
+        </div>
+        <div class="logo"><a href="#"><i class="fab fa-facebook"></i></i></a><a href="#"><i class="fab fa-twitter-square"></i></a><a href="#"><i class="fab fa-snapchat"></i></a><a href="#"><i class="fab fa-instagram-square"></i></a></div>
+    </footer>
+</body>
+
+</html>
